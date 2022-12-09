bed796730f1217dce7b62b112ddc0c50d5607523
jai wardhan <jai.wardhan2007@gmail.com>
2022-12-09 23:26:21 +0000

    END POINT ON NEW Port

--- src/main/java/com/eirtechportal/controller/testController.java
+++ src/main/java/com/eirtechportal/controller/testController.java
@@ -3,26 +3,20 @@ package com.eirtechportal.controller;
 import com.eirtechportal.models.Employee;
 import com.eirtechportal.service.RabbitMQSender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Component
+@RestControllerEndpoint(id = "rest-end-point")
+public class testController {
 
-@RestController
-@RequestMapping(value = "/javainuse-rabbitmq/")
-public class RabbitMQWebController {
 
-	@Autowired
-	RabbitMQSender rabbitMQSender;
-
-	@GetMapping(value = "/producer")
-	public String producer(@RequestParam("empName") String empName,@RequestParam("empId") String empId) {
-	
-	Employee emp=new Employee();
-	emp.setEmpId(empId);
-	emp.setEmpName(empName);
-		rabbitMQSender.send(emp);
+	@GetMapping(value = "/test")
+	public String producer1() {
 		return "Message sent to the RabbitMQ JavaInUse Successfully";
 	}
 
